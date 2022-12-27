5a6bb384a5f9fe98f2028c0211cf2071d8aba439
Tuan (Andrew) Tran <37162093+AndrewTran3003@users.noreply.github.com>
2022-12-27 16:43:53 +1100

    add model classes

---
+++ ExerciseApi/Models/Exercise Components/Equipment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ExerciseApi.Models.ExerciseComponents
+{
+	public class Equipment
+	{
+		public Guid Id { get; set; }
+		public String? Name { get; set; }
+		public String? Description { get; set; }
+		public float Price { get; set; }
+	}
+}
+
---
+++ ExerciseApi/Models/WorkoutDay.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ExerciseApi.Models
+{
+	public class WorkoutDay
+	{
+        public List<ExerciseComponent>? WorkoutPlan { get; set; }
+        public bool Completed { get; set; }
+    }
+}
+
