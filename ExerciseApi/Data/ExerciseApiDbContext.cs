fa7224d9668af3879e8a96456bf19f4527252ba5
Tuan (Andrew) Tran <37162093+AndrewTran3003@users.noreply.github.com>
2022-12-27 17:10:12 +1100

    Add Postgres and EF Core

---
+++ ExerciseApi/Data/ExerciseApiDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ExerciseApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExerciseApi.Data
+{
+	public class ExerciseApiDbContext:DbContext
+	{
+		public DbSet<WorkoutDay> WorkoutDays { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(CONNECTION_STRING);
+			base.OnConfiguring(optionsBuilder);
+		}
+
+        private const string CONNECTION_STRING = "Host=localhost;" +
+            "Port=5455;" + // THIS!!!!!
+            "Username=postgresUser;" +
+            "Password=postgresPW;" +
+            "Database=postgresDB";
+    }
+}
+
--- ExerciseApi/Models/WorkoutDay.cs
+++ ExerciseApi/Models/WorkoutDay.cs
@@ -3,6 +3,9 @@ namespace ExerciseApi.Models
 {
 	public class WorkoutDay
 	{
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public String? Description { get; set; }
         public List<ExerciseComponent>? WorkoutPlan { get; set; }
         public bool Completed { get; set; }
     }
