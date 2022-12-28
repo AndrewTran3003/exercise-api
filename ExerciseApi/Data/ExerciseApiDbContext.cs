deaba8e488f576953912c80e6dfa65c32a312607
Tuan (Andrew) Tran <37162093+AndrewTran3003@users.noreply.github.com>
2022-12-28 22:48:00 +1100

    add methods to connect to Postgres

--- ExerciseApi/Data/ExerciseApiDbContext.cs
+++ ExerciseApi/Data/ExerciseApiDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ExerciseApi.Models;
+using ExerciseApi.Models.ExerciseComponents;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExerciseApi.Data
@@ -14,11 +15,24 @@ namespace ExerciseApi.Data
 			base.OnConfiguring(optionsBuilder);
 		}
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+            modelBuilder.Entity<WorkoutDay>(e => e.ToTable("workoutday"));
+			modelBuilder.Entity<Exercise>(e => e.ToTable("exercise"));
+			modelBuilder.Entity<Break>(e => e.ToTable("break"));
+			modelBuilder.Entity<Equipment>(e => e.ToTable("equiment"));
+			modelBuilder.Entity<ExerciseRep>(e => e.ToTable("exerciserep"));
+			modelBuilder.Entity<ExerciseStep>(e => e.ToTable("exercisestep"));
+			modelBuilder.Entity<Equipment>(e => e.ToTable("equipment"));
+			base.OnModelCreating(modelBuilder);
+
+		}
+
         private const string CONNECTION_STRING = "Host=localhost;" +
-            "Port=5455;" +
-            "Username=postgresUser;" +
-            "Password=postgresPW;" +
-            "Database=postgresDB";
+            "Port=5432;" +
+            "Username=exercise-admin;" +
+            "Password=1234;" +
+            "Database=exerciseDb";
     }
 }
 
