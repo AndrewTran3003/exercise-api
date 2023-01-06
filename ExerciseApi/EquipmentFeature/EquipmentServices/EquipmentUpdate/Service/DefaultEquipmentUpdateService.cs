142f53552e32526616b9c279750fae89ad493f15
Tuan (Andrew) Tran <37162093+AndrewTran3003@users.noreply.github.com>
2023-01-06 21:04:40 +1100

    fix the issue with not found equipment

--- ExerciseApi/EquipmentFeature/EquipmentServices/EquipmentUpdate/Service/DefaultEquipmentUpdateService.cs
+++ ExerciseApi/EquipmentFeature/EquipmentServices/EquipmentUpdate/Service/DefaultEquipmentUpdateService.cs
@@ -1,3 +1,4 @@
+using ExerciseApi.EquipmentFeature.EquipmentServices.EquipmentFetcher.Service;
 using ExerciseApi.EquipmentFeature.EquipmentServices.EquipmentUpdate.Repository;
 using ExerciseApi.EquipmentFeature.Models;
 using ExerciseApi.Helpers;
@@ -7,9 +8,13 @@ namespace ExerciseApi.EquipmentFeature.EquipmentServices.EquipmentUpdate.Service
     public class DefaultEquipmentUpdateService : IEquipmentUpdateService
     {
         private readonly IEquipmentUpdateRepository _repo;
-        public DefaultEquipmentUpdateService(IEquipmentUpdateRepository repo)
+        private readonly IEquipmentFetcherService _equipmentFetcher;
+        public DefaultEquipmentUpdateService(
+            IEquipmentUpdateRepository repo,
+            IEquipmentFetcherService equipmentFetcher)
         {
             _repo = repo;
+            _equipmentFetcher = equipmentFetcher;
         }
         public async Task<OperationResult<object>> UpdateEquipmentAsync(EquipmentEntity equipment)
         {
@@ -20,6 +25,11 @@ namespace ExerciseApi.EquipmentFeature.EquipmentServices.EquipmentUpdate.Service
             if (String.IsNullOrEmpty(equipment.Id.ToString()) || String.IsNullOrWhiteSpace(equipment.Id.ToString()))
             {
                 return new OperationResult<object>(OperationStatus.Error, "The Id is required", null);
+            }
+            var equipmentToUpdate = await _equipmentFetcher.GetEquipmentByIdAsync(equipment.Id.ToString());
+            if (equipmentToUpdate.Status != OperationStatus.Success)
+            {
+                return new OperationResult<object>(OperationStatus.NotFound, $"There is no equipment with Id {equipment.Id.ToString()}", null);
             }
             return await _repo.UpdateAsync(equipment);
         }
