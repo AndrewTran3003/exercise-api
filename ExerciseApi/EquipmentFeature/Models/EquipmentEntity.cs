e662ab34bcd99194f51420a3b33e6338c5cd1ad7
Tuan (Andrew) Tran <37162093+AndrewTran3003@users.noreply.github.com>
2023-01-08 19:51:11 +1100

    add createdby and datecreated

--- ExerciseApi/EquipmentFeature/Models/EquipmentEntity.cs
+++ ExerciseApi/EquipmentFeature/Models/EquipmentEntity.cs
@@ -4,6 +4,5 @@ namespace ExerciseApi.EquipmentFeature.Models
 {
     public class EquipmentEntity : BaseEquipment
     {
-
     }
 }
---
+++ ExerciseApi/Migrations/20230108042412_add-audit-information.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExerciseApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addauditinformation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "WorkoutDays",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "WorkoutDays",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "TimeComponent",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "TimeComponent",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "ExerciseSteps",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "ExerciseSteps",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Exercises",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "Exercises",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "ExerciseReps",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "ExerciseReps",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "BasicActions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "BasicActions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "BaseEquipments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "BaseEquipments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "WorkoutDays");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "WorkoutDays");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "TimeComponent");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "TimeComponent");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "ExerciseSteps");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "ExerciseSteps");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Exercises");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "Exercises");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "ExerciseReps");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "ExerciseReps");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "BasicActions");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "BasicActions");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "BaseEquipments");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "BaseEquipments");
+        }
+    }
+}
